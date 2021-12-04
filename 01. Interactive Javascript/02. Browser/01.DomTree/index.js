70ea0680cbf435a2cc47b9070818e7e4aaee794e
yuriLee <yuriuniv@gmail.com>
2021-12-04 22:53:02 +0900

    add Browser Folder (Dom 트리)

---
+++ 01. Interactive Javascript/02. Browser/01.DomTree/index.js
@@ -0,0 +1,16 @@
+// DOM 트리 살펴보기
+const myTag = document.querySelector('#list-1');
+
+console.log(myTag);
+
+// 형제 요소 노드
+console.log(myTag.previousElementSibling);
+console.log(myTag.nextElementSibling);
+
+// 부모 요소 노드
+console.log(myTag.parentElement);
+
+// 자식 요소 노드
+console.log(myTag.children[1]);
+console.log(myTag.firstElementChild);
+console.log(myTag.lastElementChild);
